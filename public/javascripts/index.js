bf471dd32367a1671716dc1e03451554457b658f
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-04-09 22:30:30 +0430

    fixing advertisement bug

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,17 @@
 var editor = new FroalaEditor('#example')
 
-document.onreadystatechange = function () {
-    var state = document.readyState;
-    if (state == 'interactive') {
-        console.log("getting document ready");
-    } else if (state == 'complete') {
-        document.querySelector("#example .fr-wrapper div").style.display = "none"
+// document.onreadystatechange = function () {
+//     var state = document.readyState;
+//     if (state == 'interactive') {
+//         console.log("getting document ready");
+//     } else if (state == 'complete') {
+        
+//     }
+// }​
 
-    }
-  }​;
+(function() {
+    // your page initialization code here
+    // the DOM will be available here
+    document.querySelector("#example .fr-wrapper div").style.display = "none"
+ 
+ })();
